97fa230208599118865dab20d166790022be6d14
mittulmandhan <conanmittul@gmail.com>
2019-10-23 19:46:57 +0530

    sum of 2 integers

---
+++ Basic codes/sum2Int.java
@@ -0,0 +1,14 @@
+class Sum2Int {
+	int a=10;
+	int b=20;
+	
+	int sum() {
+		int c=a+b;
+		return c;
+	}
+	
+	public static void main(String []args) {
+		Sum2Int obj=new Sum2Int();
+		System.out.println(obj.sum());
+	}
+};
