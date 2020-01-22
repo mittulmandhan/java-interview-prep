b7f46571d306971dcd207e597d2c75c4e86c373b
mittulmandhan <conanmittul@gmail.com>
2020-01-22 21:08:16 +0530

    one more example added

--- 8 Heap/heap_example_codes/src/main/java/playingwithobjects/B5.java
+++ 8 Heap/heap_example_codes/src/main/java/playingwithobjects/B5.java
@@ -2,5 +2,26 @@ package playingwithobjects;
 
 // compile time error due to non initialized reference variable b
 public class B5 {
+	int x=10;
 	
+	B5 ab(B5 b /*b=103*/, C c /*c=101*/) {
+		b.x=c.y;// 103.x=101.c or 103.x=60
+		b=new B5();// b=104
+		return b;// return 104
+	}
+	
+	public static void main(String[] args) {
+		C c=new C();// c=101
+		c.y=60;// 101.y=60
+		
+		// b=102
+		//102.ab(103,101)
+		B5 b=new B5().ab(new B5(),c);// b=104
+		System.out.println(b.x);// 10
+		System.out.println(c.y);// 60
+	}
+}
+
+class C {
+	int y=20;
 }
