db5663a0ebf78e47db2aad722d11741a7b8d7fec
mittulmandhan <conanmittul@gmail.com>
2020-01-26 17:59:12 +0530

    more comments added in stackoverflow code

--- 8 Heap/heap_example_codes/src/main/java/stackoverflow/A.java
+++ 8 Heap/heap_example_codes/src/main/java/stackoverflow/A.java
@@ -6,7 +6,9 @@ public class A {
 	// new object is created in heap
 	// then that object will create new object too, to initialize its instance level reference variable
 	// this process will go on and infinite objects will be created
-	// which i turn will give stack overflow error as we get in recursion
+	// calling too many constructors in turn will give us stack overflow error at runtime as we get in recursion
+	// In this case too many constructors are loaded on stack and too many objects are created in heap as well
+	// but stack will get overflow since stack size is significantly smaller than heap size
 	A a = new A();
 	int x=10;
 	
