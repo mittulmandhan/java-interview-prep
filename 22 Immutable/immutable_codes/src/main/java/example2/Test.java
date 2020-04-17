f5a6da04f24d15b9bb6d2718554b86c3c231160a
mittulmandhan <conanmittul@gmail.com>
2020-04-17 09:53:01 +0530

    immutable codes updated

--- 22 Immutable/immutable_codes/src/main/java/example2/Test.java
+++ 22 Immutable/immutable_codes/src/main/java/example2/Test.java
@@ -6,6 +6,11 @@ public class Test {
 		System.out.println(s1.getStudentName());
 		System.out.println(s1.getStudentRoll());
 
+		// this doesnt break immutability,
+		// we assigned a new object to s1 reference variable
+		// now old object is unreachable
+		s1 = new Student("pawan", 23);
+
 		Student s2 = new Student("Bheem", 11);
 		System.out.println(s2.getStudentName());
 		System.out.println(s2.getStudentRoll());
